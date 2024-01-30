fa713ca34d47c640e9403aa402d5b4ee1ef5c57f
FilipStraka <straka168@gmail.com>
2024-01-30 22:25:21 +0100

    fill function generate_equation with variables, sum of numbers and display that numbers

--- root/scripts/add.js
+++ root/scripts/add.js
@@ -5,7 +5,17 @@ const audio = document.getElementById("myAudio");
 var answer = 0;
 
 function generate_equation () {
+    var num1 = Math.floor(Math.random() * 13 );
+    var num2 = Math.floor(Math.random() * 13 );
+    var dummyAnswer1 = Math.floor(Math.random() * 13 );
+    var dummyAnswer2 = Math.floor(Math.random() * 13 );
+    var allAnswers = [];
+    var switchAnswers = [];
 
+    answer = num1 + num2;
+
+    document.getElementById("num1").innerHTML = num1;
+    document.getElementById("num2").innerHTML = num2;
 }
 
 option1.addEventListener("click", function() {
