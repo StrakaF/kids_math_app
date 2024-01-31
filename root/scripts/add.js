492f1b3cb399da43a648c93410dda46c15d7a21f
FilipStraka <straka168@gmail.com>
2024-01-31 22:37:55 +0100

    added sound for correct answer

--- root/scripts/add.js
+++ root/scripts/add.js
@@ -1,7 +1,8 @@
 const option1 = document.getElementById("option1");
 const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
-const audio = document.getElementById("myAudio");
+const wrongAudio = document.getElementById("wrongAudio");
+const correctAudio = document.getElementById("correctAudio");
 var answer = 0;
 
 function generate_equation () {
@@ -31,24 +32,27 @@ function generate_equation () {
 option1.addEventListener("click", function() {
     if(option1.innerHTML == answer) {
         generate_equation();
+        correctAudio.play();
     } else {
-        audio.play();
+        wrongAudio.play();
     }
 });
 
 option2.addEventListener("click", function() {
     if(option2.innerHTML == answer) {
         generate_equation();
+        correctAudio.play();
     } else {
-        audio.play();
+        wrongAudio.play();
     }
 });
 
 option3.addEventListener("click", function() {
     if(option3.innerHTML == answer) {
         generate_equation();
+        correctAudio.play();
     } else {
-        audio.play();
+        wrongAudio.play();
     }
 });
 
